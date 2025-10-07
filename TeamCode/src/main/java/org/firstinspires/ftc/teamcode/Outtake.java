cfb5dddf35bb6b67d7f507e8c0311ca4a268384b
Java-Dragons <javadragonsrobotics@gmail.com>
2025-10-07 19:53:21 -0400

    Miles-encoders
    I added encoders, changed the motor types to DcMotorEx, and changed speed for velocity. needs testing.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outtake.java
@@ -1,12 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Outtake {
-    private DcMotor OuttakeRM = null;
-    private DcMotor OuttakeLM = null;
+    private DcMotorEx OuttakeRM = null;
+    private DcMotorEx OuttakeLM = null;
+
+    static final double     COUNTS_PER_MOTOR_REV    = 28 ;
 
     private LinearOpMode opmode = null;
 
@@ -16,8 +20,8 @@ public class Outtake {
         opmode = opMode;
         hwMap = opMode.hardwareMap;
 
-        OuttakeRM = hwMap.dcMotor.get("OuttakeRM");
-        OuttakeLM = hwMap.dcMotor.get("OuttakeLM");
+        OuttakeRM = hwMap.get(DcMotorEx.class, "OuttakeRM");
+        OuttakeLM = hwMap.get(DcMotorEx.class, "OuttakeLM");
 
         // LEFT motor is backwards for now
         OuttakeRM.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -26,15 +30,23 @@ public class Outtake {
         // power set to 0 as usual
         OuttakeRM.setPower(0);
         OuttakeLM.setPower(0);
+
+        OuttakeRM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        OuttakeLM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
-    public void outtake(){
-        // starts the outtake motors when called
-        OuttakeRM.setPower(1);
-        OuttakeLM.setPower(1);
+
+    public void outtake() {
+
+        // starts the outtake motors when called ^w^
+        OuttakeRM.setVelocity(2800);
+        OuttakeLM.setVelocity(2800);
     }
-    public void outtakestop(){
+
+    public void outtakestop() {
         // unsurprisingly stops the outtake
         OuttakeRM.setPower(0);
         OuttakeLM.setPower(0);
+
+
     }
 }
