9bdd0e1d4095f26e5c731d1426b128bed5d2db0d
Java-Dragons <javadragonsrobotics@gmail.com>
2025-12-09 17:43:53 -0500

    Setup  mecunum movement code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Training.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Training.java
@@ -1,9 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.GamepadStates;
+
 
 public class Training {
 
@@ -25,12 +30,19 @@ private LinearOpMode opmode = null;
         opmode = opMode;
         hwMap = opMode.hardwareMap;
 
+        GamepadStates newGamePad1 = new GamepadStates(gamepad1);
+        GamepadStates newGamePad2 = new GamepadStates(gamepad2);
+
         // name motor objects here
         FrontRM = hwMap.dcMotor.get("FrontRM");
         FrontLM = hwMap.dcMotor.get("FrontLM");
         BackRM = hwMap.dcMotor.get("BackRM");
         BackLM = hwMap.dcMotor.get("BackLM");
 
+        double axial = gamepad1.left_stick_y;
+        double lateral = gamepad1.left_stick_x;
+        double yaw = gamepad1.right_stick_x;
+        double max;
 
         // directions!!!
         FrontLM.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -39,11 +51,27 @@ private LinearOpMode opmode = null;
         BackRM.setDirection(DcMotorSimple.Direction.FORWARD);
 
 
-        // set motor powers to 0
-        FrontRM.setPower(0);
-        FrontLM.setPower(0);
-        BackRM.setPower(0);
-        BackLM.setPower(0);
+        double FrontLMPower = (axial + lateral + yaw);
+        double FrontRMPower = (axial - lateral - yaw);
+        double BackLMPower = (axial - lateral + yaw);
+        double BackRMPower = (axial + lateral - yaw);
+
+        FrontLM.setPower(FrontLMPower);
+        FrontRM.setPower(FrontRMPower);
+        BackLM.setPower(BackLMPower);
+        BackRM.setPower(BackRMPower);
+
+        max = Math.max(Math.abs(FrontLMPower), Math.abs(FrontRMPower));
+        max = Math.max(max, Math.abs(BackLMPower));
+        max = Math.max(max, Math.abs(BackRMPower));
+
+        if (max > 1.0) {
+            FrontLMPower /= max;
+            FrontRMPower /= max;
+            BackLMPower /= max;
+            BackRMPower /= max;
+        }
+
     }
 
     public void forward(double speed) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -1,8 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.opMode;
+
 import android.app.Activity;
 import android.view.View;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.LLStatus;
@@ -10,8 +14,10 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotor;
 //import com.bylazar.
-
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.teamcode.GamepadStates;
 
@@ -51,6 +57,17 @@ public class teleop extends LinearOpMode {
         Color.init(this);
         //List fiducialResult;
 
+        //    Create motor objects here
+        DcMotor FrontRM = null;
+        DcMotor FrontLM = null;
+        DcMotor BackRM = null;
+        DcMotor BackLM = null;
+        LinearOpMode opmode = null;
+
+        HardwareMap hwmap;
+        opmode = (LinearOpMode) opMode;
+        hwmap = opMode.hardwareMap;
+
 
         //limelight = hardwareMap.get(Limelight3A.class, "limelight");
 //        telemetry.setMsTransmissionInterval(11);
@@ -72,35 +89,75 @@ public class teleop extends LinearOpMode {
             newGamePad2.updateState();
 
 
-//            LL.detectPattern();
 
-            // controls movement
-            if (gamepad1.left_stick_y < -.4) {
-                // run the forward function from Functions program
-                Train.forward(speed);
-            } else if (gamepad1.left_stick_y > .4) {
-                // backwards
-                Train.backwards(speed);
-            } else if (gamepad1.left_stick_x <-.4) {
-                Train.StraffLeft(speed);
-            } else if (gamepad1.left_stick_x > .4) {
-                Train.StraffRight(speed);
-            }else if (gamepad1.right_stick_x < -.4) {
-                // left
-                Train.left(speed);
-            } else if (gamepad1.right_stick_x > .4) {
-                // right
-                Train.right(speed);
-            } else {
-                // run the stop function from training
-                Train.stop();
+            // name motor objects here
+            FrontRM = hwmap.dcMotor.get("FrontRM");
+            FrontLM = hwmap.dcMotor.get("FrontLM");
+            BackRM = hwmap.dcMotor.get("BackRM");
+            BackLM = hwmap.dcMotor.get("BackLM");
+
+            double axial = gamepad1.left_stick_y;
+            double lateral = gamepad1.left_stick_x;
+            double yaw = gamepad1.right_stick_x;
+            double max;
+
+            // directions!!!
+            FrontLM.setDirection(DcMotorSimple.Direction.REVERSE);
+            FrontRM.setDirection(DcMotorSimple.Direction.FORWARD);
+            BackLM.setDirection(DcMotorSimple.Direction.REVERSE);
+            BackRM.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+            double FrontLMPower = (axial + lateral + yaw);
+            double FrontRMPower = (axial - lateral - yaw);
+            double BackLMPower = (axial - lateral + yaw);
+            double BackRMPower = (axial + lateral - yaw);
+
+            FrontLM.setPower(FrontLMPower);
+            FrontRM.setPower(FrontRMPower);
+            BackLM.setPower(BackLMPower);
+            BackRM.setPower(BackRMPower);
+
+            max = Math.max(Math.abs(FrontLMPower), Math.abs(FrontRMPower));
+            max = Math.max(max, Math.abs(BackLMPower));
+            max = Math.max(max, Math.abs(BackRMPower));
+
+            if (max > 1.0) {
+                FrontLMPower /= max;
+                FrontRMPower /= max;
+                BackLMPower /= max;
+                BackRMPower /= max;
             }
 
+//            LL.detectPattern();
 
-
-            //initialize speed as a variable
-            telemetry.addData("Speed: ", speed);
-            telemetry.update();
+//            // controls movement
+//            if (gamepad1.left_stick_y < -.4) {
+//                // run the forward function from Functions program
+//                Train.forward(speed);
+//            } else if (gamepad1.left_stick_y > .4) {
+//                // backwards
+//                Train.backwards(speed);
+//            } else if (gamepad1.left_stick_x <-.4) {
+//                Train.StraffLeft(speed);
+//            } else if (gamepad1.left_stick_x > .4) {
+//                Train.StraffRight(speed);
+//            }else if (gamepad1.right_stick_x < -.4) {
+//                // left
+//                Train.left(speed);
+//            } else if (gamepad1.right_stick_x > .4) {
+//                // right
+//                Train.right(speed);
+//            } else {
+//                // run the stop function from training
+//                Train.stop();
+//            }
+//
+//
+//
+//            //initialize speed as a variable
+//            telemetry.addData("Speed: ", speed);
+//            telemetry.update();
 
 //            if (Color.isGreen()) {
 //                relativeLayout.setBackgroundColor(Color.greenV());
@@ -146,23 +203,22 @@ public class teleop extends LinearOpMode {
             }
 
 
-
-            // speed control
-            // decrease drive speed variable
-            if (newGamePad1.left_bumper.released) {
-                // when left bumper is pressed, slows down movement universally
-                speed -= (0.1);
-                if (speed <= 0.1) {
-                    speed = 0.1;
-                }
-            //increase speed variable
-            } else if (newGamePad1.right_bumper.released) {
-                // when right bumper is pressed, speeds up movement universally
-                speed += (0.1);
-                if (speed >= 1) {
-                    speed = (1);
-                }
-            }
+//            // speed control
+//            // decrease drive speed variable
+//            if (newGamePad1.left_bumper.released) {
+//                // when left bumper is pressed, slows down movement universally
+//                speed -= (0.1);
+//                if (speed <= 0.1) {
+//                    speed = 0.1;
+//                }
+//            //increase speed variable
+//            } else if (newGamePad1.right_bumper.released) {
+//                // when right bumper is pressed, speeds up movement universally
+//                speed += (0.1);
+//                if (speed >= 1) {
+//                    speed = (1);
+//                }
+//            }
 //            LLResult result = limelight.getLatestResult();
 //            LL.getResult();
         }
