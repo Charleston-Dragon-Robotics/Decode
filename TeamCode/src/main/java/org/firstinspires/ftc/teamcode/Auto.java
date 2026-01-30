1b9a6f4808fd4fb9b2384111737a965a8a4e6a1a
Java-Dragons <javadragonsrobotics@gmail.com>
2026-01-30 17:33:22 -0500

    modifactions for pedro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,6 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import android.media.metrics.PlaybackErrorEvent;
+
+import com.pedropathing.ftc.InvertedFTCCoordinates;
+import com.pedropathing.ftc.PoseConverter;
+import com.pedropathing.paths.callbacks.PoseCallback;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -49,35 +56,50 @@ public class Auto extends LinearOpMode {
 
     private int pathState;
 
-    public enum PathState{
-       // START POSITION I GUESS ALSO END POSITION I DONT KNOW WHAT THIS GUY IS TALKING ABOUT
+    public enum PathState {
+        // START POSITION I GUESS ALSO END POSITION I DONT KNOW WHAT THIS GUY IS TALKING ABOUT
         // DRIVE > MOVEMENT
     }
 
-    private final Pose start= new Pose(63,7,Math.toRadians(180));
-    private final Pose score= new Pose(31,110,Math.toRadians(135));
+    private final Pose start = new Pose(62, -15, Math.toRadians(180));
+    private final Pose score = new Pose(-48, -48, Math.toRadians(135));
 
-    public void buildPaths(){
-        scorePre=follower.pathBuilder()
-                .addPath(new BezierLine(start, score))
-                .setLinearHeadingInterpolation(start.getHeading(), score.getHeading())
+    private Path scorePre;
+    private PathChain grab1, score1;
+
+    public void buildPaths() {
+        scorePre = new Path(new BezierLine(start, score));
+        scorePre.setLinearHeadingInterpolation(start.getHeading(), score.getHeading());
+
+        grab1 = follower.pathBuilder()
+                .addPath(new BezierLine(score, start))
+                .setLinearHeadingInterpolation(score.getHeading(), start.getHeading())
                 .build();
-
     }
-    public void setPathState(int pState){
+
+    public void stateSelect() {
+        switch (pathState) {
+            case 0:
+                follower.followPath(scorePre, true);
+                if (!follower.isBusy()) {
+                    setPathState(1);
+                }
+                break;
+            case 1:
+                if (!follower.isBusy()) {
+                    follower.followPath(grab1);
+                }
+                if (!follower.isBusy()) {
+                    setPathState(2);
+                }
+                break;
+        }
+    }
+
+    public void setPathState(int pState) {
         pathState = pState;
         pathTimer.resetTimer();
-        pathTimer = new Timer();
-        opModeTimer = new Timer();
-        opModeTimer.resetTimer();
-
-
-        follower = Constants.createFollower(hardwareMap);
-        buildPaths();
-        follower.setStartingPose(start);
     }
-
-
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -99,38 +121,66 @@ public class Auto extends LinearOpMode {
         Launch.init(this);
         Fun.init(this);
 
+        pathTimer = new Timer();
+        opModeTimer = new Timer();
+        opModeTimer.resetTimer();
+        follower = Constants.createFollower(hardwareMap);
+        buildPaths();
+        follower.setStartingPose(start);
+
 
 //        follower.setStartingPose(new Pose());
 //
 //        poseHistory = follower.getPoseHistory();
 
-        boolean red = false;
-        boolean done = false;
+//        boolean red = false;
+//        boolean done = false;
 
         telemetry.addLine("press B if red alliance");
         telemetry.update();
 
         GamepadStates newGamPad2 = new GamepadStates(gamepad2);
 
-        while(!done) {
 
-            newGamPad2.updateState();
-
-            if (newGamPad2.b.released) {
-                red = true;
-            }
-
-            if(newGamPad2.a.released) {
-                done  = true;
-            }
-        }
+//        while(!done) {
+//
+//            newGamPad2.updateState();
+//
+//            if (newGamPad2.b.released) {
+//                red = true;
+//            }
+//
+//            if(newGamPad2.a.released) {
+//                done  = true;
+//            }
+//        }
 
         //code for the auto phase
         waitForStart();
+
+
 //
 //        Fun.forwardForDist(12, .5);
 
         while (opModeIsActive()) {
+            follower.update();
+            stateSelect();
+
+
+//            while (follower.isBusy()){
+//                    telemetry.addLine("follower is busy");
+//                follower.followPath(scorePre);
+//            }
+//            break;
+
+
+//            setPathState();
+//            telemetry.addLine("Path built");
+//            telemetry.update();
+//            follower.followPath(scorePre);
+//            telemetry.addLine("Path followed");
+//            telemetry.update();
+
 //        Drive.forward(1);
 //        sleep(500);
 //        Drive.stop();
@@ -183,16 +233,16 @@ public class Auto extends LinearOpMode {
 //            Intake.FeedStop();
 //            Launch.stop();
 //            sleep(500);
-            if(red==true) {
-//                Drive.StraffRight(.5);
-//                sleep(1000);
-//                Drive.stop();
-            } else if (red==false) {
-//                Drive.StraffLeft(.5);
-//                sleep(1000);
-//                Drive.stop();
-            }
-            break;
+//            if(red==true) {
+////                Drive.StraffRight(.5);
+////                sleep(1000);
+////                Drive.stop();
+//            } else if (red==false) {
+////                Drive.StraffLeft(.5);
+////                sleep(1000);
+////                Drive.stop();
+//            }
+//            break;
         }
     }
 }
