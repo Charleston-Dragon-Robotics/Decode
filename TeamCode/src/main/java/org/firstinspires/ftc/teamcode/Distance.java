a6279f5354e81849f1fe8b906c0ef911dd82413d
Java-Dragons <javadragonsrobotics@gmail.com>
2026-03-31 20:00:57 -0400

    started work on a pidf linearslide challeng

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Distance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Distance.java
@@ -1,4 +1,38 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Distance {
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.opMode;
+
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+
+public class Distance{
+
+    private DistanceSensor SD;
+
+    private LinearOpMode opMode = null;
+
+    public Distance() {}
+
+    public void init(LinearOpMode opMode) {
+        HardwareMap hwMap;
+
+        hwMap = opMode.hardwareMap;
+
+        SD = hardwareMap.get(Rev2mDistanceSensor.class, "sensor_distance");
+
+    }
+
+    public double measureD(){
+        return(SD.getDistance(DistanceUnit.INCH));
+    }
+
+
+
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Linearslide.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Linearslide {
+
+    private DcMotor Slide = null;
+
+private LinearOpMode opMode = null;
+
+    public Linearslide(){}
+}
